72d3db82e17eb729c1256ad835277b810058e82c
Pankaj Lohumi <pankaj.lohumi@in.fcm.travel>
2026-05-30 13:09:30 +0530

    revert: Top-Down Vanshavali redesign

---
+++ bump_version_29.py
@@ -0,0 +1,15 @@
+import glob
+import re
+
+html_files = glob.glob('*.html')
+
+for file_name in html_files:
+    with open(file_name, 'r', encoding='utf-8') as f:
+        content = f.read()
+    
+    content = re.sub(r'js/([^"]+)\?v=28', r'js/\1?v=29', content)
+    
+    with open(file_name, 'w', encoding='utf-8') as f:
+        f.write(content)
+
+print("Bumped version strings to v=29.")
